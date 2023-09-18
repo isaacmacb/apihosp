ee47803bcfe3d235583933be3d7d4a9ffc8fbee8
isaacmacb <liberdadebessa@gmail.com>
2023-09-18 05:54:55 -0300

    adicionando méetodos

--- app.js
+++ app.js
@@ -1,13 +1,31 @@
 const express = require('express');
 const app = express();
+const path = require('path');
+
+const loginRoutes = require('./routes/login'); // Importe as rotas relacionadas ao login
+const cadastroRoutes = require('./routes/cadastro'); // Importe as rotas relacionadas ao cadastro
+
 const port = 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
-app.use(express.static(__dirname + '/public'));
+app.set('views', path.join(__dirname, 'views'));
+app.use(express.static(path.join(__dirname, 'public')));
 
+// Rota para a página inicial
 app.get('/', (req, res) => {
-  res.render('index');
+  res.render('index'); // Renderize a visualização "index.ejs"
+});
+
+// Use as rotas relacionadas ao login definidas em login.js
+app.use(loginRoutes);
+
+// Use as rotas relacionadas ao cadastro definidas em cadastro.js
+app.use(cadastroRoutes);
+
+// Rota para a página de cadastro
+app.get('/cadastro', (req, res) => {
+  res.render('cadastro');
 });
 
 app.listen(port, () => {
